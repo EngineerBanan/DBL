29cb5cc1cff3f8dc424ab8bc2071feffeb6fa115
EngineerBanan <legrand.gatien77@gmail.com>
2024-07-14 01:19:19 +0200

    rajouts d'images, essai de changement de description tag

--- scripts.js
+++ scripts.js
@@ -2,17 +2,18 @@ const myImg = document.getElementById("personnage");
 const myButton = document.getElementById("toggleImage");
 
 let isFirstImage = true;
-let isGogeta = false;
 
 myButton.addEventListener("click", function() {
   if (isFirstImage) {
     banniere.src = "images/banniere_vegeta.webp";
     personnage.src = "images/changement_tag_active.webp";
     fusion.src = "images/fusion_off.webp";
-  } else {
+    updateText();
+  } else if (!isFirstImage) {
     banniere.src = "images/banniere_goku.webp";
     personnage.src = "images/changement_tag.webp";
     fusion.src = "images/fusion_off.webp";
+    updateText();
   }
   isFirstImage = !isFirstImage;
 });
@@ -20,10 +21,10 @@ myButton.addEventListener("click", function() {
 const myImg2 = document.getElementById("fusion");
 const myButton2 = document.getElementById("transfo");
 
-let isSameImage = false;
+let isGogeta = false;
 
 myButton2.addEventListener("click", function() {
-  if (isSameImage) {
+  if (isGogeta) {
     banniere.src = "images/banniere_goku.webp";
     fusion.src = "images/fusion_off.webp";
   } else {
@@ -32,51 +33,57 @@ myButton2.addEventListener("click", function() {
     personnage.src = "images/changement_tag.webp";
 
   }
-  isSameImage = !isSameImage;
+  isGogeta = !isGogeta;
+  updateText();
 });
 
-myButton.addEventListener("click", function() {
-  if (myImg = "images/banniere_goku"){
-    document.getElementById('principal').innerHTML =
-    '<p>'+
-    'Cède la place à « Vegeta » via une substitution.'+
-    '<br>'+
-    'Si vous avez 3 cartes ou moins, tire 1 carte au hasard.'+
-    '<br>'+
-    'Regénère le ki de 50.'+
-    '<br>'+
-    'Regénère 50% de la jauge de téléportation (se déclenche 2 fois).'+
-    '<br>'+
-    'Octroie un bonus d\'état annulant les actions spéciales qui s\'activent'+
-    '<br>'+
-    'lors d\'une mise à couvert de l\'adversaire pendant 5 secondes (se déclenche 2 fois).'+
-    '<br>'+
-    'Annule les malus de stats.'+
-    '<br>'+
-    '<br>'+
-    'Condition d\'utilisation : jauge de substitution pleine.'+
-    '</p>';
-  } else if (myImg = "images/banniere_vegeta") {
+function updateText() {
+  const banniereSrc = banniere.src;
+  if (banniereSrc == "images/banniere_goku.webp") {
     document.getElementById("principal").innerHTML =
-    '<p>'+
-    'Cède la place à « Goku » via une substitution.'+
-    '<br>'+
-    'Si vous avez 3 cartes ou moins, tire 1 carte au hasard.'+
-    '<br>'+
-    'Regénère le ki de 50.'+
-    '<br>'+
-    'Regénère 50% de la jauge de téléportation (se déclenche 2 fois).'+
-    '<br>'+
-    'Octroie un bonus d\'état annulant les actions spéciales qui s\'activent'+
-    '<br>'+
-    'lors d\'une mise à couvert de l\'adversaire pendant 5 secondes (se déclenche 2 fois).'+
-    '<br>'+
-    'Annule les malus de stats.'+
-    '<br>'+
-    '<br>'+
-    'Condition d\'utilisation : jauge de substitution pleine.'+
-    '</p>';
-  } else if(myImg = "images/banniere_gogeta") {
+    "<p class="/light/">"+
+    "Cède la place à « Vegeta » via une substitution."+
+    "<br>"+
+    "Si vous avez 3 cartes ou moins, tire 1 carte au hasard."+
+    "<br>"+
+    "Regénère le ki de 50."+
+    "<br>"+
+    "Regénère 50% de la jauge de téléportation (se déclenche 2 fois)."+
+    "<br>"+
+    "Octroie un bonus d'état annulant les actions spéciales qui s'activent"+
+    "<br>"+
+    "lors d'une mise à couvert de l'adversaire pendant 5 secondes (se déclenche 2 fois)."+
+    "<br>"+
+    "Annule les malus de stats."+
+    "<br>"+
+    "<br>"+
+    "</p>"+
+    "<p>"+
+    "Condition d'utilisation : jauge de substitution pleine."+
+    "</p>";
+  } else if (banniereSrc == "images/banniere_vegeta.webp") {
+    document.getElementById("principal").innerHTML =
+    "<p class="/light/">"+
+    "Cède la place à « Son Goku » via une substitution."+
+    "<br>"+
+    "Si vous avez 3 cartes ou moins, tire 1 carte au hasard."+
+    "<br>"+
+    "Regénère le ki de 50."+
+    "<br>"+
+    "Regénère 50% de la jauge de téléportation (se déclenche 2 fois)."+
+    "<br>"+
+    "Octroie un bonus d'état annulant les actions spéciales qui s'activent"+
+    "<br>"+
+    "lors d'une mise à couvert de l'adversaire pendant 5 secondes (se déclenche 2 fois)."+
+    "<br>"+
+    "Annule les malus de stats."+
+    "<br>"+
+    "<br>"+
+    "</p>"+
+    "<p>"+
+    "Condition d'utilisation : jauge de substitution pleine."+
+    "</p>";
+  } else if (banniereSrc == "images/banniere_gogeta.webp") {
     document.getElementById("principal").innerHTML =
     '<p>'+
     'Tire une carte d\'art spécial au tour suivant.'+
@@ -94,5 +101,6 @@ myButton.addEventListener("click", function() {
     'Condition d\'utilisation : après 15 secondes.'+
     '</p>';
   }
-});
+}
 
+myButton.addEventListener("click", updateText);
